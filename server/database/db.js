4d1db4cdbde22595eeb36ce406ae35f222d38cc6
Roshan <roshankishore17@gmail.com>
2023-02-02 03:36:17 +0530

    initial commit

---
+++ server/database/db.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const USERNAME = process.env.DB_USERNAME;
+const PASSWORD = process.env.DB_PASSWORD;
+
+const Connection = () => {
+
+    const MONGODB_URI = `mongodb://${USERNAME}:${PASSWORD}@ac-pxwt5pm-shard-00-00.jfrywmq.mongodb.net:27017,ac-pxwt5pm-shard-00-01.jfrywmq.mongodb.net:27017,ac-pxwt5pm-shard-00-02.jfrywmq.mongodb.net:27017/?ssl=true&replicaSet=atlas-n8luu5-shard-0&authSource=admin&retryWrites=true&w=majority`;
+
+    mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+    mongoose.connection.on('connected', () => {
+        console.log('Database connected Successfully');
+    })
+
+    mongoose.connection.on('disconnected', () => {
+        console.log('Database disconnected');
+    })
+
+    mongoose.connection.on('error', () => {
+        console.log('Error while connecting with the database ', error.message);
+    })
+}
+
+export default Connection;
